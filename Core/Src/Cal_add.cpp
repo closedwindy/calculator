75ed28d7e77b0e39500dd8b2c26eebce6d710b98
closewindy <qseppo@163.com>
2025-03-09 18:22:37 +0800

    玛卡巴卡

---
+++ Core/Src/Cal_add.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hhw on 2025/3/9.
+//
+
+#include "Cal_add.h"
---
+++ Core/Src/Cal_get.cpp
@@ -0,0 +1,13 @@
+//
+// Created by hhw on 2025/3/9.
+//
+
+#include "Cal_get.h"
+
+void CAL_GET(int num,int place)
+{
+   static int array[8];
+   array[place] = num;
+}
+
+
